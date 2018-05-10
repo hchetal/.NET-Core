c164658758304e313ef592ea5b66be8e756e5234
Himanshu Chetal <himanshu.chetal@outlook.com>
2018-05-10 20:53:35 +0800

    Checking the startup.cs file

--- CityInfo.API/src/CityInfo.API/Startup.cs
+++ CityInfo.API/src/CityInfo.API/Startup.cs
@@ -27,10 +27,19 @@ namespace CityInfo.API
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
+                app.UseExceptionHandler();
+            }
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            app.Run((context) =>
+            {
+                throw new Exception("This is example exception");
             });
         }
     }
